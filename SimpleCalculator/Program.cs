2f3cf42a087eb961b5cb8a4bbcafa3dbe35ea633
Obe <o.bdondo@gmail.com>
2020-07-14 19:50:57 -0700

    Created subtract method

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -26,5 +26,10 @@ namespace SimpleCalculator
             return sum;
         }
 
+        public int subtract(int num1, int num2)
+        {
+            int sum = num1 - num2;
+            return sum;
+        }
     }
 }
